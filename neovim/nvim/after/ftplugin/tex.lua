35252e44aa75f77c7bb95a5db9ab64328c990280
timoteo dinelli <timoteo.dinelli@polimi.it>
2024-02-27 06:40:08 -0800

    Add filetype configuration and spelling

--- neovim/nvim/after/ftplugin/tex.lua
+++ neovim/nvim/after/ftplugin/tex.lua
@@ -1,2 +1,8 @@
+vim.opt.tabstop = 3
+vim.opt.softtabstop = 3
+vim.opt.shiftwidth = 3
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.textwidth=90
 vim.opt.wrap = true
 vim.opt.spell = true
